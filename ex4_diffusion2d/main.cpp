d4786439757170d1ed92d1ff7598f624874daaa5
Florian Mahlknecht <fmahlknecht@student.ethz.ch>
2019-11-16 22:40:33 +0100

    Adds mpi helloworld project (ex4) with CMakeLists

---
+++ ex4_diffusion2d/main.cpp
@@ -0,0 +1,18 @@
+#include <openmpi/mpi.h>
+#include <iostream>
+
+
+int main(int argc, char *argv[]) {
+  int rank, size;
+  MPI_Init(&argc, &argv);
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  std::cout << "Hello from rank " << rank << " out of " << size << std::endl;
+
+
+  MPI_Finalize();
+
+  return 0;
+}
