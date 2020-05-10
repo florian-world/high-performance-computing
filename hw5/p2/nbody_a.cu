d1da364f75688c222234e13ebf1366a6b9843813
Florian Mahlknecht <m@florian.world>
2020-05-10 05:12:05 +0200

    Revert "Compiler keyword fix for aliasing"
    This reverts commit 9ef685ba58f9b0480dbbf2d644d6fdd99e9058e0.

--- hw5/p2/nbody_a.cu
+++ hw5/p2/nbody_a.cu
@@ -1,13 +1,13 @@
 #include <cuda_runtime.h>
 
-__global__ void computeForcesKernel(int N, const double3 *p, double3 * __restrict__ f) {
+__global__ void computeForcesKernel(int N, const double3 *p, double3 *f) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx >= N)
         return;
 
     // TODO: Copy the code from `nbody_0.cu` and fix the redundant memory accesses.
 
-    f[idx] = double3{0.0, 0.0, 0.0};
+    double3 tmp{0.0, 0.0, 0.0};
     for (int i = 0; i < N; ++i) {
         double dx = p[i].x - p[idx].x;
         double dy = p[i].y - p[idx].y;
@@ -16,10 +16,11 @@ __global__ void computeForcesKernel(int N, const double3 *p, double3 * __restric
         // by zero. (dx * inv_r will be exactly 0.0)
         double r = sqrt(1e-150 + dx * dx + dy * dy + dz * dz);
         double inv_r = 1 / r;
-        f[idx].x += dx * inv_r * inv_r * inv_r;
-        f[idx].y += dy * inv_r * inv_r * inv_r;
-        f[idx].z += dz * inv_r * inv_r * inv_r;
+        tmp.x += dx * inv_r * inv_r * inv_r;
+        tmp.y += dy * inv_r * inv_r * inv_r;
+        tmp.z += dz * inv_r * inv_r * inv_r;
     }
+    f[idx] = tmp;
 }
 
 void computeForces(int N, const double3 *p, double3 *f) {
