e09c6cd0a251279d71d316afdea88fd38e13c73d
Florian Mahlknecht <m@florian.world>
2020-05-10 03:56:00 +0200

    Implements memory benchmarks

--- hw5/p1/benchmarks_b.cu
+++ hw5/p1/benchmarks_b.cu
@@ -9,8 +9,45 @@ static constexpr int kBufferSizes[] = {
     5'000'001, 20'000'001, 50'000'001,
 };
 
+__global__ void copyKernelABP(int K, double *a, const int* p, const double*b) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < K) {
+        a[idx] = b[p[idx]];
+    }
+}
+
+__global__ void copyKernelAPB(int K, double *a, const int* p, const double*b) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < K) {
+        a[p[idx]] = b[idx];
+    }
+}
+
+__global__ void addKernelAAB(int K, double *a, const double*b) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < K) {
+        a[idx] = a[idx] + b[idx];
+    }
+}
+
+__global__ void addKernelAAB100(int K, double *a, const double*b) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < K) {
+        for (int i = 0; i < 100; ++i)
+            a[idx] = a[idx] + b[idx];
+    }
+}
+
+
+
+
 void subtask_b() {
-    constexpr int threadsPerBlock = 1024;
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, 0);
+    const int threadsPerBlock = prop.maxThreadsPerBlock;
+
+    printf("Using device with %d threads per block\n", threadsPerBlock);
+
     int maxK = kBufferSizes[sizeof(kBufferSizes) / sizeof(kBufferSizes[0]) - 1];
 
     /// Pick a N with respect to K such that total running time is more or less uniform.
@@ -45,7 +82,7 @@ void subtask_b() {
         // TODO: Measure the execution time of synchronously uploading K doubles from the host to the device. Report GB/s
 
         double dt = benchmark(pickN(K), [aDev, aHost, K](){
-            cudaMemcpy(aDev, aHost, K * sizeof(double), cudaMemcpyHostToDevice);
+            CUDA_CHECK(cudaMemcpy(aDev, aHost, K * sizeof(double), cudaMemcpyHostToDevice));
         });
 
         double gbps = K*sizeof(double) / dt / 1e9; // Gigabytes per second here;
@@ -62,12 +99,17 @@ void subtask_b() {
             permutationFunc(K);
 
             /// TODO: Copy pHost to pDev. Don't forget CUDA_CHECK.
+            CUDA_CHECK(cudaMemcpy(pDev, pHost, K * sizeof(int), cudaMemcpyHostToDevice));
 
             /// TODO: Benchmark the a_i = b_{p_i} kernel.
-            double dtABP = 0.0;
+            double dtABP = benchmark(pickN(K), [threadsPerBlock, K, aDev, bDev, pDev]() {
+                CUDA_LAUNCH(copyKernelABP, (K+threadsPerBlock-1) / threadsPerBlock, threadsPerBlock, K, aDev, pDev, bDev);
+            });
 
             /// TODO: (OPTIONAL) Benchmark the a_{p_i} = b_i kernel;
-            double dtAPB = 0.0;
+            double dtAPB = benchmark(pickN(K), [threadsPerBlock, K, aDev, bDev, pDev]() {
+                CUDA_LAUNCH(copyKernelAPB, (K+threadsPerBlock-1) / threadsPerBlock, threadsPerBlock, K, aDev, pDev, bDev);
+            });
 
             // Report how many bytes per second was written.
             printf("Case %s  -->  K=%8d  [a=b_p] %6.2f GB/s  [a_p=b] %6.2f GB/s written\n",
@@ -107,14 +149,20 @@ void subtask_b() {
     // Task 1b.3) and 1b.4)
     for (int K : kBufferSizes) {
         // TODO: Benchmark a_i += b_i kernel.
-        double dt1 = 0.0;
+        double dt1 = benchmark(pickN(K), [threadsPerBlock, K, aDev, bDev]() {
+            CUDA_LAUNCH(addKernelAAB, (K+threadsPerBlock-1) / threadsPerBlock, threadsPerBlock, K, aDev, bDev);
+        });;
 
         // TODO: Benchmark the kernel that repeats a_i += b_i 100x times.
-        double dt100 = 0.0;
+        double dt100 = benchmark(pickN(K), [threadsPerBlock, K, aDev, bDev]() {
+            CUDA_LAUNCH(addKernelAAB100, (K+threadsPerBlock-1) / threadsPerBlock, threadsPerBlock, K, aDev, bDev);
+        });;
 
-        double gflops1 = 0.0;
-        double gflops100 = 0.0;
-        printf("a+b  1x -> %4.1f GFLOP/s  100x -> %5.1f GFLOP/s\n", gflops1, gflops100);
+        // printf("dts = %f, %f\n", dt1, dt100);
+
+        double gflops1 = K*1E-9/dt1;
+        double gflops100 = K*100*1E-9/dt100;
+        printf("a+b  -->  K=%8d  1x -> %4.1f GFLOP/s  100x -> %5.1f GFLOP/s\n", K, gflops1, gflops100);
     }
 
 
