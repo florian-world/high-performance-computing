b45b4ed61bd9a773c9143032d3f0d5064d3be7ef
Florian Mahlknecht <m@florian.world>
2020-05-10 06:24:08 +0200

    Attempt to fix shared memory

--- hw5/p2/nbody_d.cu
+++ hw5/p2/nbody_d.cu
@@ -1,43 +1,39 @@
 #include <cuda_runtime.h>
 
 __global__ void computeForcesKernel(int N, const double3 *p, double3 *f) {
-    const size_t block_lb = blockIdx.x * blockDim.x;
-    const size_t tidx = threadIdx.x;
-    const size_t idx = block_lb + threadIdx.x;
-    const size_t block_ub = block_lb + blockDim.x;
-    if (idx >= N)
-        return;
+    const size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    double3 myp = idx < N ? p[idx] : double3{0.0, 0.0, 0.0};
+
+    extern __shared__ double3 s[];
 
     // TODO: Copy the code from `nbody_c.cu` and utilize shared memory.
-    extern __shared__ double3 s[];
-    s[tidx] = p[idx];
-
-    __syncthreads();
-
     double3 tmp{0.0, 0.0, 0.0};
-    for (int i = 0; i < N; ++i) {
-        double dx;
-        double dy;
-        double dz;
+    for (int i = 0; i < N; i+= blockDim.x) {
 
-        if (block_lb <= i && i < block_ub) {
-            dx = s[i-block_lb].x - s[tidx].x;
-            dy = s[i-block_lb].y - s[tidx].y;
-            dz = s[i-block_lb].z - s[tidx].z;
-        } else {
-            dx = p[i].x - s[tidx].x;
-            dy = p[i].y - s[tidx].y;
-            dz = p[i].z - s[tidx].z;
+        // read into shared memory
+        if (i+threadIdx.x < N) s[threadIdx.x] = p[i+threadIdx.x];
+
+        __syncthreads();
+
+        // work in shared memory from now on
+        for (int j = 0; j < min(blockDim.x, N-i); ++j) {
+            double dx = s[j].x - myp.x;
+            double dy = s[j].y - myp.y;
+            double dz = s[j].z - myp.z;
+            // Instead of skipping the i == idx case, add 1e-150 to avoid division
+            // by zero. (dx * inv_r will be exactly 0.0)
+            double inv_r = rsqrt(1e-150 + dx * dx + dy * dy + dz * dz);
+            double inv_r_3 = inv_r*inv_r*inv_r;
+            tmp.x += dx * inv_r_3;
+            tmp.y += dy * inv_r_3;
+            tmp.z += dz * inv_r_3;
         }
-        // Instead of skipping the i == idx case, add 1e-150 to avoid division
-        // by zero. (dx * inv_r will be exactly 0.0)
-        double inv_r = rsqrt(1e-150 + dx * dx + dy * dy + dz * dz);
-        double inv_r_3 = inv_r*inv_r*inv_r;
-        tmp.x += dx * inv_r_3;
-        tmp.y += dy * inv_r_3;
-        tmp.z += dz * inv_r_3;
+
+        __syncthreads();
     }
-    f[idx] = tmp;
+
+    if (idx < N) f[idx] = tmp;
 }
 
 void computeForces(int N, const double3 *p, double3 *f) {
@@ -46,6 +42,6 @@ void computeForces(int N, const double3 *p, double3 *f) {
 
     // TODO: Set the required shared memory size.
     //       Don't bother with checking errors here.
-    size_t shmSize = numThreads * sizeof(double)*3; // 3 doubles per thread  
+    size_t shmSize = numThreads * sizeof(double3); // 3 doubles per thread  
     computeForcesKernel<<<numBlocks, numThreads, shmSize>>>(N, p, f);
 }
