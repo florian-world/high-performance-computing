776760f14094cb2aa0d6ef5b6be96c83a4e8d95d
Florian Mahlknecht <m@florian.world>
2020-05-10 05:01:51 +0200

    Simple fix for aliasing

--- hw5/p2/nbody_a.cu
+++ hw5/p2/nbody_a.cu
@@ -6,6 +6,21 @@ __global__ void computeForcesKernel(int N, const double3 *p, double3 *f) {
         return;
 
     // TODO: Copy the code from `nbody_0.cu` and fix the redundant memory accesses.
+
+    double3 tmp{0.0, 0.0, 0.0};
+    for (int i = 0; i < N; ++i) {
+        double dx = p[i].x - p[idx].x;
+        double dy = p[i].y - p[idx].y;
+        double dz = p[i].z - p[idx].z;
+        // Instead of skipping the i == idx case, add 1e-150 to avoid division
+        // by zero. (dx * inv_r will be exactly 0.0)
+        double r = sqrt(1e-150 + dx * dx + dy * dy + dz * dz);
+        double inv_r = 1 / r;
+        tmp.x += dx * inv_r * inv_r * inv_r;
+        tmp.y += dy * inv_r * inv_r * inv_r;
+        tmp.z += dz * inv_r * inv_r * inv_r;
+    }
+    f[idx] = tmp;
 }
 
 void computeForces(int N, const double3 *p, double3 *f) {
